86386c707d1bed4aeca6e85f5e5dcf22c91096fe
Diogo96ferreira <diogo96ferreira@gmail.com>
2022-11-14 17:57:53 +0000

    Cleanup

--- ExpensesTracker/src/router/index.ts
+++ ExpensesTracker/src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import AuthView from '../views/AuthView.vue';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: () => import('../views/AuthView.vue')
+
     },
     {
       path: '/about',
